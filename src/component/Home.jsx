b184ec3ba36e5d8237c2d9e211b62e3b9edaca8f
CelticSgc80 <sankhajitghoshchowdhury@gmail.com>
2023-06-11 00:33:36 +0530

    second commit

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
   return (
     <Layout >
       <div className='homemain'>
-        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
           <div class="carousel-indicators">
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
