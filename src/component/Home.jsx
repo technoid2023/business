2697320996061d4f22ec0d73bedf952f2acfa052
subham04mitra <subhammitra58@gmail.com>
2023-06-11 20:38:42 +0530

    home update

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -31,44 +31,14 @@ const Home = () => {
   return (
     <Layout >
       <div className='homemain'>
-        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-          <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-          </div>
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img src="assests/service2.jpg" class="d-block w-100" alt="..." />
-              <div class="carousel-caption d-none d-md-block">
-                <button className='btn btn-danger' onClick={() => { document.location.assign('/quote') }}>Get Quote</button>
-                <p>Some representative placeholder content for the first slide.</p>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <img src="assests/home1.jpg" class="d-block w-100" alt="..." />
-              <div class="carousel-caption d-none d-md-block">
-                <button className='btn btn-warning' onClick={() => { document.location.assign('/quote') }}>Get Quote</button>
-                <p>Some representative placeholder content for the second slide.</p>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <img src="assests/company3.jpg" class="d-block w-100" alt="..." />
-              <div class="carousel-caption d-none d-md-block">
-                <button className='btn btn-info' onClick={() => { document.location.assign('/quote') }}>Get Quote</button>
-                <p>Some representative placeholder content for the third slide.</p>
-              </div>
-            </div>
-          </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
-        </div>
+      
+      <img src="assests/home1.jpg"   style={{ height: "28rem" }}class="hero-image" />
+  <div class="hero-text">
+    <h1 style={{fontsize:'50px',color:'darkblue'}}>Welcome to Tech World</h1>
+    <p style={{color:'brown'}}>Be OverPowerd with Online UPS</p>
+    <button>Get Quote</button>
+
+</div>
 
 
         {/* carousel container ended */}
